5c2b9b13098ed43af5c2bf385bb02aaf904fabb2
FabioBassani <avilar.fabio@yahoo.com.br>
2022-05-19 16:17:15 +0200

    .

---
+++ index.js
@@ -0,0 +1,17 @@
+const sayHello = () => {
+    const sayMyName = () => {
+        
+    }
+    console.log('hello world')
+}
+const name = 'Fabio';
+
+const name2 = 'Ronald';
+
+const lastName = 'Bassani';
+
+
+
+
+sayHello()
+
